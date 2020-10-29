7087e985ea9c83714973bec30c70e8e5f5101ed1
auxfuse <aux.fuse@yahoo.com>
2020-10-29 20:50:13 +0000

    Rendered default hackathon_view view as proof of concept

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("<int:hack_id>/team/<int:team_id>/judging/", judging, name="judging"),
     path("<int:hack_id>/final_score", check_projects_scores, name="final_score"),
     path("create_hackathon", create_hackathon, name='create_hackathon'),
+<<<<<<< HEAD
     path("<int:hackathon_id>/", view_hackathon,
          name='view_hackathon'),
     path("<int:hackathon_id>/update", update_hackathon,
@@ -23,5 +24,13 @@ urlpatterns = [
     path("<int:hackathon_id>/delete", delete_hackathon,
          name="delete_hackathon"),
     path('enroll/', enroll_toggle, name='enroll_toggle'),
+=======
+    path("view_hackathon", view_hackathon, name='view_hackathon'),
+    path("<int:hackathon_id>/update_hackathon", update_hackathon, name="update_hackathon"),
+    path("<int:hackathon_id>/delete_hackathon", delete_hackathon, name="delete_hackathon"),
+    path('<int:pk>/',
+         HackathonDetailView.as_view(), name='hackathon_detail'),
+    path('enroll', enroll_toggle, name='enroll_toggle')
+>>>>>>> Rendered default hackathon_view view as proof of concept
 ]
 
