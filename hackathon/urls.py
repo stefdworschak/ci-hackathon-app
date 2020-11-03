3cafedb995dec1d34b7add5abb7ffb2f94b7d249
auxfuse <aux.fuse@yahoo.com>
2020-11-03 00:25:10 +0000

    Hackathon view template structure definied with styles applied as per wireframe

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
     path("<int:hack_id>/team/<int:team_id>/judging/", judging, name="judging"),
     path("<int:hack_id>/final_score", check_projects_scores, name="final_score"),
     path("create_hackathon", create_hackathon, name='create_hackathon'),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path("<int:hackathon_id>/", view_hackathon,
          name='view_hackathon'),
     path("<int:hackathon_id>/update", update_hackathon,
@@ -25,17 +23,5 @@ urlpatterns = [
     path("<int:hackathon_id>/delete", delete_hackathon,
          name="delete_hackathon"),
     path('enroll/', enroll_toggle, name='enroll_toggle'),
-=======
-    path("view_hackathon", view_hackathon, name='view_hackathon'),
-=======
-    path("<int:hackathon_id>/view_hackathon", view_hackathon,
-         name='view_hackathon'),
->>>>>>> Hackathon returned via get_object_or_404 in views.py function, and default test rendered in hackathon-view.html template
-    path("<int:hackathon_id>/update_hackathon", update_hackathon, name="update_hackathon"),
-    path("<int:hackathon_id>/delete_hackathon", delete_hackathon, name="delete_hackathon"),
-    path('<int:pk>/',
-         HackathonDetailView.as_view(), name='hackathon_detail'),
-    path('enroll', enroll_toggle, name='enroll_toggle')
->>>>>>> Rendered default hackathon_view view as proof of concept
 ]
 
